18692b14eae7f4bc825f0bf8e9979d96cbd4d7a8
tmwcl <tmwclaxton@gmail.com>
2023-12-18 23:18:59 +0000

    deviously hitting grobid :{}

--- internal/dispatcher/worker.go
+++ internal/dispatcher/worker.go
@@ -3,15 +3,42 @@ package dispatcher
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"simple-go-app/internal/envHelper"
+	"simple-go-app/internal/grobid"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
+var (
+	lastRequestTime   time.Time
+	lastRequestTimeMu sync.Mutex
+)
+
 func Worker(id int, messageQueue <-chan *sqs.Message, svc *sqs.SQS, sqsURL string, s3Bucket string) {
+
+	awsRegion := envHelper.GetEnvVariable("AWS_REGION")
+
+	minGapBetweenRequests := envHelper.GetEnvVariable("MINIMUM_GAP_BETWEEN_REQUESTS_SECONDS")
+	minGap, err := time.ParseDuration(minGapBetweenRequests + "s")
+	if err != nil {
+		log.Fatalf("Error parsing MINIMUM_GAP_BETWEEN_REQUESTS_SECONDS: %v", err)
+	}
+
 	log.Printf("Starting worker %d...\n", id)
+	// Create an AWS session with the specified region
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion),
+	}))
+	s3Svc := s3.New(sess)
+
 	for {
 		message := <-messageQueue
 
@@ -26,9 +53,60 @@ func Worker(id int, messageQueue <-chan *sqs.Message, svc *sqs.SQS, sqsURL strin
 
 		fmt.Printf("Worker %d received message. Path: %s. Operation: %s\n", id, path, operation)
 
-		time.Sleep(time.Minute)
+		// Ensure a minimum gap between requests
+		lastRequestTimeMu.Lock()
+		timeSinceLastRequest := time.Since(lastRequestTime)
+		lastRequestTimeMu.Unlock()
 
-		_, err := svc.ChangeMessageVisibility(&sqs.ChangeMessageVisibilityInput{
+		if timeSinceLastRequest < minGap {
+			sleepTime := minGap - timeSinceLastRequest
+			log.Printf("Worker %d sleeping for %v to meet the minimum gap between requests\n", id, sleepTime)
+			time.Sleep(sleepTime)
+		}
+
+		// Download the file from S3
+		output, err := s3Svc.GetObject(&s3.GetObjectInput{
+			Bucket: aws.String(s3Bucket),
+			Key:    aws.String(path),
+		})
+		if err != nil {
+			log.Println("Error downloading file from S3:", err)
+			log.Printf("Bucket: %s, Key: %s\n", s3Bucket, path)
+			continue
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				fmt.Println("Error closing S3 response body:", err)
+			}
+		}(output.Body)
+
+		fileContent, err := ioutil.ReadAll(output.Body)
+		if err != nil {
+			log.Println("Error reading file content:", err)
+			continue
+		}
+
+		// Send the file content to the Grobid service
+		response, err := grobid.SendPDF2Grobid(fileContent)
+
+		// print a snippet of the response
+		fmt.Println(string(response[0:100]))
+
+		if err != nil {
+			log.Println("Error sending file to Grobid service:", err)
+		}
+
+		// Update the last request time
+		lastRequestTimeMu.Lock()
+		lastRequestTime = time.Now()
+		lastRequestTimeMu.Unlock()
+
+		// Change message visibility or delete the message from the SQS queue
+		// based on your application logic
+
+		// For example, changing message visibility:
+		_, err = svc.ChangeMessageVisibility(&sqs.ChangeMessageVisibilityInput{
 			QueueUrl:          aws.String(sqsURL),
 			ReceiptHandle:     message.ReceiptHandle,
 			VisibilityTimeout: aws.Int64(30),
--- internal/grobid/grobid.go
+++ internal/grobid/grobid.go
@@ -1,9 +1,12 @@
 package grobid
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"simple-go-app/internal/envHelper"
 	"strconv"
@@ -17,10 +20,10 @@ func CheckGrobidHealth(healthStatus *bool, healthMutex *sync.Mutex, fn ...func()
 	healthMutex.Unlock()
 	grobidHostname := "grobid"
 	grobidPort := "8070"
-	grobidURL := fmt.Sprintf("http://%s:%s", grobidHostname, grobidPort)
+	GrobidURL := fmt.Sprintf("http://%s:%s", grobidHostname, grobidPort)
 	healthEndpoint := "/api/isalive"
 	// Attempt to make a GET request to the Grobid health endpoint
-	resp, err := http.Get(grobidURL + healthEndpoint)
+	resp, err := http.Get(GrobidURL + healthEndpoint)
 	if err != nil {
 		fmt.Println("Error checking Grobid health:", err)
 		*healthStatus = false
@@ -50,4 +53,59 @@ func CheckGrobidHealth(healthStatus *bool, healthMutex *sync.Mutex, fn ...func()
 	}
 	fmt.Println("Setting Grobid health status to", isHealthy)
 	*healthStatus = isHealthy
+}
+
+func SendPDF2Grobid(fileContent []byte) ([]byte, error) {
+	// Create a buffer to store the multipart form data
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+
+	// Add the file field to the request
+	fileField, err := writer.CreateFormField("input")
+	if err != nil {
+		return nil, err
+	}
+
+	// Copy the file content to the form file field
+	_, err = io.Copy(fileField, bytes.NewReader(fileContent))
+	if err != nil {
+		return nil, err
+	}
+
+	// Add other form fields
+	err = writer.WriteField("consolidateHeader", "true")
+	if err != nil {
+		return nil, err
+	}
+
+	// Close the multipart writer
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// Make a POST request to the Grobid service endpoint
+	resp, err := http.Post("http://grobid:8070/api/processFulltextDocument", writer.FormDataContentType(), &requestBody)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Error closing Grobid response body:", err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("grobid service returned non-OK status: %v", resp.Status)
+	}
+
+	// Read Grobid service response
+	grobidResponse, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("Grobid successfully processed the file")
+	if err != nil {
+		return nil, err
+	}
+
+	return grobidResponse, nil
 }
